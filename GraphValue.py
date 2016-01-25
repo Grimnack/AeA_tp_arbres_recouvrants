85b6511dfa9b202d93ec6fb4d67e85fc0feaad90
Matthieu Caron <matthieu.caron@laposte.net>
2016-01-25 11:19:47 +0100

    init

---
+++ GraphValue.py
@@ -0,0 +1,23 @@
+import random as r
+
+class GraphValue(object):
+    """docstring for GraphValue"""
+    def __init__(self, matrix, ):
+        super(GraphValue, self).__init__()
+        self.voisins = []
+        for ligne in matrix :
+            self.voisins.append(ligne[:])
+        self.poids = []
+        for ligne in poids :
+            self.poids.append(ligne[:])
+
+
+
+def graphAlea(n,p) :
+    matrix = []
+    for i in range(n) :
+        matrix.append([False]*n)
+    for i in range(n) :
+        if p <= r.random() :
+
+    
