b878cbaf4f578bcac811ca08ffbf0b1f0f64c771
Matthieu Caron <matthieu.caron@laposte.net>
2016-01-25 11:43:09 +0100

    alea done

--- GraphValue.py
+++ GraphValue.py
@@ -2,7 +2,7 @@ import random as r
 
 class GraphValue(object):
     """docstring for GraphValue"""
-    def __init__(self, matrix, ):
+    def __init__(self, matrix, poids):
         super(GraphValue, self).__init__()
         self.voisins = []
         for ligne in matrix :
@@ -15,9 +15,25 @@ class GraphValue(object):
 
 def graphAlea(n,p) :
     matrix = []
+    poids = []
+    N = n**4 # puissance 4
     for i in range(n) :
         matrix.append([False]*n)
+        poids.append([-1]*n)
     for i in range(n) :
-        if p <= r.random() :
+        for j in range(i+1,n) :
+            if p >= r.random() :
+                matrix[i][j] = True
+                matrix[j][i] = True
+    for i in range(n) :
+        for j in range(i+1,n) :
+            if matrix[i][j] :
+                poids[i][j] = r.randint(1,N)
+                poids[j][i] = poids[i][j]
+
+    return GraphValue(matrix,poids)
+
+graph = graphAlea(4,0.5)
+print(graph.poids)
 
     
